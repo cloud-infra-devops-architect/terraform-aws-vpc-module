c05071450c17f69459bcf3792e5c444723af33ab
Pratik Das <awscloudinfraautomation@gmail.com>
2026-05-25 02:02:06 -0500

    initial commit of aws vpc module in feat/dev branch

--- terraform.tf
+++ terraform.tf
@@ -1,4 +1,5 @@
 # Your configuration does not need a cloud block for VCS-driven workspaces. HCP Terraform handles the backend automatically when you connect your VCS repository to HCP Terraform. You can remove the cloud block from your configuration, and HCP Terraform will manage the backend for you.
+
 terraform {
   required_version = "~> 1.15.0"
   required_providers {

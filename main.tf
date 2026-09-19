2ccddf716acc3af75befb469edf85312147af329
Pratik Das <cloud.infra.devops.architect@gmail.com>
2026-09-19 02:28:56 -0500

    added vpc examples folder in the vpc module

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 data "aws_availability_zones" "available" {
-  state = "available"
+  # state = "available"
 
-  filter {
-    name   = "zone-type"
-    values = ["availability-zone"]
-  }
+  # filter {
+  #   name   = "zone-type"
+  #   values = ["availability-zone"]
+  # }
 }
 
 locals {
